26eacd0a6e7f1100dec097fb707d2b33b1ee4b85
reinaldoweb <reinaldoweb@hotmail.com>
2024-09-23 20:59:53 -0300

    trabalhando com Rotas pt2

---
+++ aula/aula-app/src/pages/Error/index.js
@@ -0,0 +1,10 @@
+
+export default function Error()
+{
+    return (
+        <div>
+            <h1>Página não encontrada</h1>
+
+        </div>
+    )
+}
--- aula/aula-app/src/routes.js
+++ aula/aula-app/src/routes.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import Sobre from './pages/Sobre';
 import Contato from './pages/Contato';
+import Produto from './pages/Produto';
+import Error from './pages/Error';
 
 import Header from './components/Header';
 
@@ -14,6 +16,8 @@ const Routes = () => {
                 <Route exact path="/" component={Home} />
                 <Route exact path="/Sobre" component={Sobre} />
                 <Route exact path="/Contato" component={Contato} />
+                <Route exact path="/Produto/:id" component={Produto} />
+                <Route path="*" component={Error}/>
             </Switch>
         </BrowserRouter>
     );
